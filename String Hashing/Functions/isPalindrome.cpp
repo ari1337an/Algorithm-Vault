f3de63268609df4d8232f34a0e75d0c065aac891
Sahedul Hasan Ariyan <ari1337an@gmail.com>
2020-11-28 21:27:21 +0600

    Update isPalindrome.cpp

--- String Hashing/Functions/isPalindrome.cpp
+++ String Hashing/Functions/isPalindrome.cpp
@@ -1,3 +1,13 @@
-bool isPalindrom(StringHash x){
+// O(1)
+bool isPalindrom(StringHash x){ // For full string
 	return x.forward_hash() == x.reverse_hash();
 }
+
+// O(1)
+bool isPalindrom(StringHash x,int l,int r){
+	return x.forward_hash(l,r) == x.reverse_hash(l,r);
+}
+
+// USAGE
+string s = "abcmadamyus";
+cout << ((isPalindrom(StringHash(s),3,7))?"YES":"NO") close
