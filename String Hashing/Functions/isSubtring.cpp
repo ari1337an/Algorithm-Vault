eb479c845c1dd816ae43dfe0a068c6d22eeeffb2
Sahedul Hasan Ariyan <ari1337an@gmail.com>
2020-11-28 21:24:00 +0600

    Update isSubtring.cpp

--- String Hashing/Functions/isSubtring.cpp
+++ String Hashing/Functions/isSubtring.cpp
@@ -1,14 +1,18 @@
-// O(1)
+// O(|s|)
 bool isSubstring(StringHash s_hash, StringHash p_hash){
 	int size_of_p = p_hash.size();
 	bool possible = false;
 	for (int i = 0; i < s_hash.size(); ++i) {
 		int start = i;
 		int end = start + size_of_p - 1;
-		if(s_hash.forward_hash(start,end) == p_hash.forward_hash()){
+		if(s_hash.forward_hash(start,end) == p_hash.forward_hash()){ // O(1)
 			possible = true;
 			break;
 		}
 	}
 	return possible;
 }
+
+
+// Brute Force -> O( |s| * |p| )
+// Hashing     -> O( |s| )
