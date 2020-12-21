72f3619203a5af51d1c149f54a3f50b8118c2ea1
Sahedul Hasan Ariyan <ari1337an@gmail.com>
2020-12-21 22:15:15 +0600

    Update pattern_search.cpp

--- KMP/Application/pattern_search.cpp
+++ KMP/Application/pattern_search.cpp
@@ -1,7 +1,8 @@
+//O( |text| + |pattern| )
 int number_of_matches(string text,string pattern){
-    vector<int> LPS = prefix_function(text);
+    vector<int> LPS = prefix_function(pattern); // O( |pattern| )
     int matches = 0;
-    for (int i = 0, q = 0; i < (int)text.size(); ++i) {
+    for (int i = 0, q = 0; i < (int)text.size(); ++i) { // O( |text| )
         while(q > 0 && pattern[q] != text[i]) q = LPS[q-1];
         if(pattern[q] == text[i]) q++;
         if(q == pattern.size()){
